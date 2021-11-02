7e270c34de581c3b4f3c4f316a3ea41495ab25ff
Wahaha-007 <b0464221@gmail.com>
2021-11-03 00:00:56 +0700

    Change to play unitl who reach 5 first

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
-let score = 0;
+let myScore = 0;
+let comScore = 0;
 
-function computerPlay(){
-    
-    let randomNumber = Math.floor(Math.random()*3) + 1;
+function computerPlay() {
 
-    let result = (randomNumber==1) ? 'rock' 
-                :(randomNumber==2) ? 'paper'
-                : 'scissors';   
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
 
-    return result;        
+    let result = (randomNumber == 1) ? 'rock'
+        : (randomNumber == 2) ? 'paper'
+            : 'scissors';
+
+    return result;
 }
 
-function playRound(playerSelection, computerSelection){
-    
+function playRound(playerSelection, computerSelection) {
+
     let result;
 
-    if(playerSelection != null) playerSelection = playerSelection.toLowerCase();
+    if (playerSelection != null) playerSelection = playerSelection.toLowerCase();
     else playerSelection = '';
-    
-    
-    if(playerSelection == 'rock'){
-        if(computerSelection == 'rock')         result = 'Draw ! Rock vs Rock.';
-        else if(computerSelection == 'paper')   result = 'You lose ! Paper beats Rock.' ;
-        else                                    { result = 'You win ! Rock beats Scissors.'; score++; }
 
-    }else if(playerSelection == 'paper'){
-        if(computerSelection == 'rock')         { result = 'You Win ! Paper beats Rock.'; score++; }
-        else if(computerSelection == 'paper')   result = 'Draw ! Paper vs Paper.';
-        else                                    result = 'You lose ! Scissors beats Paper.';
 
-    }else if(playerSelection == 'scissors'){
-        if(computerSelection == 'rock')         result = 'You lose ! Rock beats Scissors.';
-        else if(computerSelection == 'paper')   { result = 'You win ! Scissors beat Paper.'; score++; }
-        else                                    result = 'Draw ! Scissors vs Scissors.';
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'rock') { result = 'Draw ! Rock vs Rock.'; }
+        else if (computerSelection == 'paper') { result = 'You lose ! Paper beats Rock.'; comScore++; }
+        else { result = 'You win ! Rock beats Scissors.'; myScore++; }
+
+    } else if (playerSelection == 'paper') {
+        if (computerSelection == 'rock') { result = 'You Win ! Paper beats Rock.'; myScore++; }
+        else if (computerSelection == 'paper') { result = 'Draw ! Paper vs Paper.'; }
+        else { result = 'You lose ! Scissors beats Paper.'; comScore++; }
+
+    } else if (playerSelection == 'scissors') {
+        if (computerSelection == 'rock') { result = 'You lose ! Rock beats Scissors.'; comScore++; }
+        else if (computerSelection == 'paper') { result = 'You win ! Scissors beat Paper.'; myScore++; }
+        else { result = 'Draw ! Scissors vs Scissors.'; }
     }
-    else{
+    else {
         result = "Don't recognize your selection !";
     }
 
@@ -42,21 +43,22 @@ function playRound(playerSelection, computerSelection){
 
 }
 
-function game(){
-    for (let i=1; i<=5;i++){
+function game() {
 
-        // const playerSelection = "rock";
+    let gResult;
 
+    do{
         const playerSelection = window.prompt("Your choice (Rock/Paper/Scissors) : ");
         const computerSelection = computerPlay();
-        
-        console.log("------ Round "+ i + " of 5 ------");
+
         console.log("Player : " + playerSelection);
         console.log("Computer : " + computerSelection);
         console.log(playRound(playerSelection, computerSelection));
-        console.log("Your current score is "+score);
-    }
+        console.log("You vs Computer is " + myScore + ":" + comScore);
+    } while ((myScore < 5)&&(comScore<5));
 
+    gResult = (myScore > comScore) ? 'You Win !' : 'You Lose';
+    console.log(gResult);
 }
 
 game();
