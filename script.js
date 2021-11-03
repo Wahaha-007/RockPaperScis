3153011d710c7a249130d3945bf61fa85e26077f
Wahaha-007 <b0464221@gmail.com>
2021-11-03 10:12:19 +0700

    First Workable GUI (with little color)

--- script.js
+++ script.js
@@ -12,13 +12,14 @@ function computerPlay() {
     return result;
 }
 
-function playRound(playerSelection, computerSelection) {
+function newPlayRound(e) {
+
+    //console.log(e.target.getAttribute('choice'));
 
     let result;
 
-    if (playerSelection != null) playerSelection = playerSelection.toLowerCase();
-    else playerSelection = '';
-
+    const playerSelection = e.target.getAttribute('choice');
+    const computerSelection = computerPlay();
 
     if (playerSelection == 'rock') {
         if (computerSelection == 'rock') { result = 'Draw ! Rock vs Rock.'; }
@@ -26,39 +27,44 @@ function playRound(playerSelection, computerSelection) {
         else { result = 'You win ! Rock beats Scissors.'; myScore++; }
 
     } else if (playerSelection == 'paper') {
-        if (computerSelection == 'rock') { result = 'You Win ! Paper beats Rock.'; myScore++; }
+        if (computerSelection == 'rock') { result = 'You win ! Paper beats Rock.'; myScore++; }
         else if (computerSelection == 'paper') { result = 'Draw ! Paper vs Paper.'; }
         else { result = 'You lose ! Scissors beats Paper.'; comScore++; }
 
-    } else if (playerSelection == 'scissors') {
+    } else { // if (playerSelection == 'scissors') {
         if (computerSelection == 'rock') { result = 'You lose ! Rock beats Scissors.'; comScore++; }
         else if (computerSelection == 'paper') { result = 'You win ! Scissors beat Paper.'; myScore++; }
         else { result = 'Draw ! Scissors vs Scissors.'; }
     }
-    else {
-        result = "Don't recognize your selection !";
+
+    const dcomSelect = document.querySelector(".comSelect");
+    dcomSelect.textContent = `Computer select : ${computerSelection}`;
+
+    const dResult = document.querySelector(".result");
+    dResult.textContent = result;
+
+    if (result.search("win") != -1) {
+        dResult.style.cssText = "color:green";
+    } else if (result.search("lose") != -1) {
+        dResult.style.cssText = "color:red";
+    }else{
+        dResult.style.cssText = "color:gray";
     }
 
-    return result;
+    const dScore = document.querySelector(".cScore");
+    dScore.textContent = `${myScore}:${comScore}`;
 
+    if (myScore >= 5) {
+        dResult.textContent = "Today is really your day !";
+        dResult.style.cssText = "color:green; font-size:50px";
+        myScore = comScore = 0;
+
+    } else if (comScore >= 5) {
+        dResult.textContent = "It's the End. Your life is up.";
+        dResult.style.cssText = "color:red; font-size:50px";
+        myScore = comScore = 0;
+    }
 }
 
-function game() {
-
-    let gResult;
-
-    do{
-        const playerSelection = window.prompt("Your choice (Rock/Paper/Scissors) : ");
-        const computerSelection = computerPlay();
-
-        console.log("Player : " + playerSelection);
-        console.log("Computer : " + computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-        console.log("You vs Computer is " + myScore + ":" + comScore);
-    } while ((myScore < 5)&&(comScore<5));
-
-    gResult = (myScore > comScore) ? 'You Win !' : 'You Lose';
-    console.log(gResult);
-}
-
-game();
+const butts = Array.from(document.querySelectorAll('.mbutton'));
+butts.forEach(key => key.addEventListener('click', newPlayRound));
